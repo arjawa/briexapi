2a341a2641dd2cade3225d8201c48e1a57edf282
briexenv <briexenv@gmail.com>
2021-11-18 10:09:25 +0800

    add more styles

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ app.get('/', async (req, res) => {
 	res.send('Hello World!')
 })
 
-app.get('/api/textpro/glitch', async (req, res) => {
+app.get('/api/textpro/:style', async (req, res) => {
+	let style = req.params.style
 	let text1 = req.query.text1
-	let text2 = req.query.text2
-	res.send({"result": await textpro(text1, text2)})
+	let text2 = req.query.text2 ? req.query.text2 : null
+	
+	res.header("Content-Type",'application/json')
+	if (!style || !text1) res.send(JSON.stringify({"status": "error", "result": "invalid parameter!"}, undefined, 4))
+	else res.send(await textpro(style, text1, text2))
 })
 
 app.listen(app.get("port"), () => {
-	console.log(`Example app listening at http://localhost:${app.get("port")}`)
+	console.log(`API service running successfully!`)
 })
