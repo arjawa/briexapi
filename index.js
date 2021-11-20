456e68d3b0e5c7b9188c6c2716aee81e7388046d
briexenv <briexenv@gmail.com>
2021-11-20 19:02:06 +0800

    upload memes

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 
+// configurations
 app.set("port", process.env.PORT || 5000);
+app.set("view engine", "pug")
 app.set("json spaces", 4);
+
+// static files
 app.use("/memes", express.static("./memes"));
-app.use("/api", require("./routes/textmaker"));
+app.use("/assets", express.static("./assets"));
+
+// routes
+app.use("/textmaker", require("./routes/textmaker"));
 app.use("/meme", require("./routes/meme"));
 
 app.get("/", async (req, res) => {
-	res.send("Hello World!");
+	res.render("index");
 });
 
 app.listen(app.get("port"), () => {
