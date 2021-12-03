50516a4427c11edc54d8e628bf2f274abe2efef2
briexenv <briexenv@gmail.com>
2021-12-03 19:08:12 +0800

    update docs

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 
 
 const memeid = async (req, res) => {
-  let dirpath = "../public/memes/";
+  let dirpath = path.join(path.resolve(__dirname, '..'), "/public/memes/");
   let memes = fs.readdirSync(dirpath);
   let random = memes[Math.floor(Math.random() * memes.length)];
   res.sendFile(dirpath + random);
